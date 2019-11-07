fdcef84eb233777358a73659c71517f1e9c4e2e9
Fabian <fabianlevinsky@gmail.com>
2019-11-07 19:30:58 -0300

    Primera Version

--- IngSoftware1/src/ingsoftware1/IngSoftware1.java
+++ IngSoftware1/src/ingsoftware1/IngSoftware1.java
@@ -5,11 +5,15 @@
  */
 package ingsoftware1;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -20,25 +24,28 @@ import javafx.stage.Stage;
 public class IngSoftware1 extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage) throws IOException {
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//            
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//        Scene scene = new Scene(root, 300, 250);
+//        primaryStage.setTitle("Hello World!");
+//        primaryStage.setScene(scene);
+//        primaryStage.show();
+          Parent root = FXMLLoader.load(getClass().getResource("Interfaz/MiVentana.fxml"));
+          Scene scene = new Scene(root);
+          primaryStage.setScene(scene);
+          primaryStage.show();
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     /**
@@ -46,6 +53,11 @@ public class IngSoftware1 extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+        System.out.println("hola mundo");
+        System.out.println("Obligatorio Ing Software");
+        System.out.println("holaaaaaaaaaaaaaaaaaaaaaa");
+        
+     
     }
     
 }
