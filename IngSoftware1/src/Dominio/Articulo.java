fa9f592225f215c155c0529f97a73575a89decfc
Dario <dariowsz@hotmail.com>
2019-11-08 10:19:49 -0300

    Se agregaron clases Articulo y Envase

---
+++ IngSoftware1/src/Dominio/Articulo.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dominio;
+
+/**
+ *
+ * @author Darío
+ */
+import java.util.ArrayList;
+
+public class Articulo {
+    
+    //Atributos
+    private String origenMatPrima;
+    private int precio;
+    private String material;
+    private String codigo;
+    private ArrayList<Envase> envasesPosibles;
+    private Envase envaseElegido;
+
+    //Constructores
+    public Articulo(String unOrigenMatPrima, int unPrecio, String unMaterial, String unCodigo, ArrayList<Envase> listaEnvasesPosibles, Envase unEnvaseElegido) {
+        origenMatPrima = unOrigenMatPrima;
+        precio = unPrecio;
+        material = unMaterial;
+        codigo = unCodigo;
+        envasesPosibles = listaEnvasesPosibles;
+        envaseElegido = unEnvaseElegido;
+    }
+
+    //Setters
+    public void setOrigenMatPrima(String origenMatPrima) {
+        this.origenMatPrima = origenMatPrima;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setEnvasesPosibles(ArrayList<Envase> envasesPosibles) {
+        this.envasesPosibles = envasesPosibles;
+    }
+
+    public void setEnvaseElegido(Envase envaseElegido) {
+        this.envaseElegido = envaseElegido;
+    }
+    
+    //Getters
+    public String getOrigenMatPrima() {
+        return origenMatPrima;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public ArrayList<Envase> getEnvasesPosibles() {
+        return envasesPosibles;
+    }
+
+    public Envase getEnvaseElegido() {
+        return envaseElegido;
+    }
+}
